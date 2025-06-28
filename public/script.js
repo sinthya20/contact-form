d98921aa2e76a0609325b979c1684c23d0d89e8b
sinthya <ssinthya2010@gmail.com>
2025-06-28 15:07:38 +0530

    Fix: linked CSS/JS and added Firebase config

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAtO0gyXI5ZYsjCg18X4M-fZZiNQuoJUOo",
   authDomain: "contactform-d9247.firebaseapp.com",
+  databaseURL: "https://contactform-d9247-default-rtdb.firebaseio.com",
   projectId: "contactform-d9247",
   storageBucket: "contactform-d9247.firebasestorage.app",
   messagingSenderId: "491500748346",
   appId: "1:491500748346:web:f30861d936e7fd1937bf03"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Handle form submission
